628dca5a7887a94d1e4840f505cdfc84db0188e2
Kako <104598739+K4kin@users.noreply.github.com>
2023-01-22 00:32:47 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,37 @@
+// o navegador ver arquivo com nome index como principal.
+
+/*
+OBJETIVO 1 - quando p usuário clicar no botão de ver trailer, devemos abrir a modal com o video do trailer
+
+- passo 1 - dar um jeito de pegar o elemento que representa o botão na tela usando o js
+- passo 2 - dar um jeito de identificar quando o susuario clicar no botao
+- passo 3 - dar um jeito de pegar o elemento da modal no js
+- passo 4 - abrir a modal na tela
+
+OBJETIVO 2 - quando o usuario clicar no X devemos fechar a modal.
+ 
+- passo 1 - dar um jeito de pegar a modal usando js
+- passo 2 - dar um jeito de identificar quando o usuario clicar no X
+- passo 3 - fechar a modal
+
+*/
+
+const botaoTrailer = document.querySelector(".botao-trailer");
+const botaoFecharModal = document.querySelector(".fechar-modal");
+const video = document.getElementById("video");
+const modal = document.querySelector(".modal");
+const linkDoVideo = video.src;
+
+function alternarModal(){
+	modal.classList.toggle("aberto");
+}
+
+botaoTrailer.addEventListener("click", () => {
+	alternarModal();
+	video.setAttribute("src", linkDoVideo);
+});
+
+botaoFecharModal.addEventListener("click", () => {
+	alternarModal();
+	video.setAttribute("src", "");
+});
